bc354490717d262ca87403dd190182a0d2be4b25
Pål Hamel <pal.hamel@gmail.com>
2020-08-24 12:17:36 +0200

    set state with search result

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,29 @@
 import React from "react";
 
-import { SearchBar } from './SearchBar'
-import youtube from '../api/youtube'
+import { SearchBar } from "./SearchBar";
+import youtube from "../api/youtube";
 
 export class App extends React.Component {
-  onTermSubmit = (term) => {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
     // console.log('from app.js:', term)
-    youtube.get('/search', {
+    const response = await youtube.get("/search", {
       params: {
         q: term,
-      }
+      },
     });
+    console.log(response);
+    // set state with response items array:
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        We have {this.state.videos.length} videos.
       </div>
-    )
+    );
   }
 }
